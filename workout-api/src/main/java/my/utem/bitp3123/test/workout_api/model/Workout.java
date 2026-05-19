1feab40faf85c8ccb98e6f6bf34b2c9c45f3eb7a
AzimAminz <anakuazim10@gmail.com>
2026-05-19 20:52:24 +0800

    Refactor: Restore strict primitive fields using global spring.jackson.deserialization.fail-on-null-for-primitives configuration

--- workout-api/src/main/java/my/utem/bitp3123/test/workout_api/model/Workout.java
+++ workout-api/src/main/java/my/utem/bitp3123/test/workout_api/model/Workout.java
@@ -1,10 +1,10 @@
 package my.utem.bitp3123.test.workout_api.model;
 
 public class Workout {
-    private Integer id;
+    private int id;
     private String exerciseName;
-    private Integer durationMinutes;
-    private Double caloriesBurned;
+    private int durationMinutes;
+    private double caloriesBurned;
     private String category;
 
     // No-arg constructor
@@ -12,7 +12,7 @@ public class Workout {
     }
 
     // All-args constructor
-    public Workout(Integer id, String exerciseName, Integer durationMinutes, Double caloriesBurned, String category) {
+    public Workout(int id, String exerciseName, int durationMinutes, double caloriesBurned, String category) {
         this.id = id;
         this.exerciseName = exerciseName;
         this.durationMinutes = durationMinutes;
@@ -21,44 +21,46 @@ public class Workout {
     }
 
     // Getters and Setters
-    public Integer getId() {
+    public int getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(int id) {
         this.id = id;
     }
 
     public String getExerciseName() {
         return exerciseName;
-      }
-  
-      public void setExerciseName(String exerciseName) {
-          this.exerciseName = exerciseName;
-      }
-  
-      public Integer getDurationMinutes() {
-          return durationMinutes;
-      }
-  
-      public void setDurationMinutes(Integer durationMinutes) {
-          this.durationMinutes = durationMinutes;
-      }
-  
-      public Double getCaloriesBurned() {
-          return caloriesBurned;
-      }
-  
-      public void setCaloriesBurned(Double caloriesBurned) {
-          this.caloriesBurned = caloriesBurned;
-      }
-  
-      public String getCategory() {
-          return category;
-      }
-  
-      public void setCategory(String category) {
-          this.category = category;
-      }
-  }
+    }
+
+    public void setExerciseName(String exerciseName) {
+        this.exerciseName = exerciseName;
+    }
+
+    public int getDurationMinutes() {
+        return durationMinutes;
+    }
+
+    public void setDurationMinutes(int durationMinutes) {
+        this.durationMinutes = durationMinutes;
+    }
+
+    public double getCaloriesBurned() {
+        return caloriesBurned;
+    }
+
+    public void setCaloriesBurned(double caloriesBurned) {
+        this.caloriesBurned = caloriesBurned;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
+
+
 
