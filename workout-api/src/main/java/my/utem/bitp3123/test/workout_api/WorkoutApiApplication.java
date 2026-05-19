9bc17c6f3add36eee0ee53367d605419c09af813
AzimAminz <anakuazim10@gmail.com>
2026-05-19 20:31:31 +0800

    update front and back

---
+++ workout-api/src/main/java/my/utem/bitp3123/test/workout_api/WorkoutApiApplication.java
@@ -0,0 +1,13 @@
+package my.utem.bitp3123.test.workout_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutApiApplication.class, args);
+	}
+
+}
